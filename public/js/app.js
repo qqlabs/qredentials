56b91821e6c85d495b82ab98ab7eb0986d279c3a
Roy Zheng <roycraft3@gmail.com>
2014-11-08 13:06:40 -0800

    angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,23 @@
 (function() {
-    var app = angular.module('qredentials', []);
+    var app = angular.module('qred-login', []);
     app.controller('LoginFormController', ['$scope', '$http', '$location', function($scope, $http, $location) {
-        this.emailTaken = 'what the ';
+        $scope.emailTaken = false;
         $scope.submit = function() {
             $http.post('http://wontonst.com', {
                 email: $scope.email,
                 password: $scope.password
             }).success(function(data, status, headers, config) {
                 console.log(response);
-                $location.path('/profile');
+                $location.path('/profile');//TODO GET PROFILES URL
             }).error(function(data, status, headers, config) {
-                $scope.emailTaken = true;
+                $scope.emailTaken = true;//TODO maybe have two errors "conn bad" and "email taken"
             });
         };
     }]);
+})();
+(function(){
+var app = angular.module('qred-profile',[]);
+app.controller('WallController',['$scope',function($scope){
+
+}]);
 })();
