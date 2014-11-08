17ccb5b72fed4d016d335bfd3636d922ef023c26
Roy Zheng <roycraft3@gmail.com>
2014-11-08 12:24:13 -0800

    angular work

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,17 @@
 (function() {
     var app = angular.module('qredentials', []);
-    app.controller('LoginFormController', ['$scope','$http', function($scope,$http) {
+    app.controller('LoginFormController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+        this.emailTaken = 'what the ';
         $scope.submit = function() {
             $http.post('http://wontonst.com', {
                 email: $scope.email,
                 password: $scope.password
-            }).success(function(data,status,headers,config){
-            console.log(response);
-			}).error(function(data,status,headers,config){
-			console.log("fail");
-			});
-        }
+            }).success(function(data, status, headers, config) {
+                console.log(response);
+                $location.path('/profile');
+            }).error(function(data, status, headers, config) {
+                $scope.emailTaken = true;
+            });
+        };
     }]);
 })();
