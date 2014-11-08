7ecc5596f456e04227d9518455e1ea53de174007
Roy Zheng <roycraft3@gmail.com>
2014-11-08 02:13:10 -0800

    login page

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,15 @@
-(function(){
-var app = angular.module('qredentials',[]);
-app.controller('LoginController',function(){
-
-});
+(function() {
+    var app = angular.module('qredentials', []);
+    app.controller('LoginFormController', ['$scope','$http', function($scope,$http) {
+        $scope.submit = function() {
+            $http.post('http://wontonst.com', {
+                email: $scope.email,
+                password: $scope.password
+            }).success(function(data,status,headers,config){
+            console.log(response);
+			}).error(function(data,status,headers,config){
+			console.log("fail");
+			});
+        }
+    }]);
 })();
