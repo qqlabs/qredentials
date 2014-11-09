cb7d3634cb0e53b04b7d3d19fe186ff28759e43f
Quentin <qhsu@usc.edu>
2014-11-09 02:23:27 -0800

    migrating qr contact to profile controller

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,30 +1,30 @@
 (function() {
     var app = angular.module('qred-profile', ['ngRoute']);
-	app.config(function($routeProvider) {
+    app.config(function($routeProvider) {
         $routeProvider.when('/', {
-                templateUrl: '/directives/profile.html',
-            })
-            .when('/my', {
-                templateUrl: '/directives/mycard.html',
-            })
-			.when('/edit',{
-			templateUrl: '/directives/edit_profile.html',
-			})
-			.when('/wall',{
-			templateUrl: '/directives/wall.html',
-			});
+            templateUrl: '/directives/profile.html',
+        })
+        .when('/my', {
+            templateUrl: '/directives/mycard.html',
+        })
+        .when('/edit',{
+         templateUrl: '/directives/edit_profile.html',
+     })
+        .when('/wall',{
+         templateUrl: '/directives/wall.html',
+     });
     });
-	app.controller('BrowseController',['$scope','$rootScope',function($scope,$rootScope){
-	$rootScope.bg={'background-image' : 'url(/img/desk.jpg)'};
-	
-	}]);
+    app.controller('BrowseController',['$scope','$rootScope',function($scope,$rootScope){
+       $rootScope.bg={'background-image' : 'url(/img/desk.jpg)'};
+
+   }]);
     app.controller('ProfileController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
-	$rootScope.bg={'background-image' : 'url(/img/desk.jpg)'};
-	$scope.$on('$includeContentLoaded',function(){
-	console.log("fuck!");
-	});
-        $scope.profile = {};
-        $scope.load = function(id) {
+       $rootScope.bg={'background-image' : 'url(/img/desk.jpg)'};
+       $scope.$on('$includeContentLoaded',function(){
+           console.log("fuck!");
+       });
+       $scope.profile = {};
+       $scope.load = function(id) {
             $http.get('&id=' + id). //TODO get URL
             success(function(data, status, headers, config) {
                 $scope.profile = data;
@@ -37,9 +37,9 @@
                     company: "HackSC",
                     title: "Senior Executive Intern",
                     addresses: [
-                        "123 Easy St.",
-                        "Apt 44",
-                        "District 13"
+                    "123 Easy St.",
+                    "Apt 44",
+                    "District 13"
                     ],
                     city: 'San Jose',
                     state: 'CA',
@@ -60,31 +60,42 @@
                     qrurl : "qred.azurewebsites.net/browse.html"
 
                 };
-				$rootScope.profile = data;
+                $rootScope.profile = data;
                 $scope.profile = data;
                 $scope.profile.address = data.addresses[0];
                 $scope.profile.phone_number = data.phone_numbers[0];
                 $scope.profile.site = data.sites[0].url;
                 $scope.profile.qrurl = "http://chart.apis.google.com/chart?cht=qr&chs=100x100&chl=" + data.qrurl;
+
+                var vc = "BEGIN:VCARD\nVERSION:3.0\n";
+                vc += "N:" + lastName + ";" + firstName + "\n";
+                vc += "FN:" + firstName + "\n";
+                vc += "EMAIL:" + email + "\n";
+                vc += "TEL;TYPE=HOME:" + phone + "\n";
+                vc += "END:VCARD";
+                vc = encodeURIComponent(vc);
+
+                $scope.profile.qrcontact = ("http://chart.apis.google.com/chart?cht=qr&chs=100x100&chl=" + vc);
+                
             });
-        };
-        $scope.load();
-    }]);
-    app.controller('WallController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
-        $scope.posts = [];
-        $scope.submit = function() {
-            $http.post('http://wontonst.com', {
+};
+$scope.load();
+}]);
+app.controller('WallController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
+    $scope.posts = [];
+    $scope.submit = function() {
+        $http.post('http://wontonst.com', {
+            content: $scope.new_post,
+            target: $rootScope.profile.email
+        }).success(function(data, status, headers, config) {
+            $scope.posts.push({
+                author: $scope.self.name,
                 content: $scope.new_post,
-                target: $rootScope.profile.email
-            }).success(function(data, status, headers, config) {
-                $scope.posts.push({
-                    author: $scope.self.name,
-                    content: $scope.new_post,
-                    pic : $scope.self.pic
-                });
-            }).error(function(data, status, headers, config) {});
-        };
-        $scope.load = function(id) {
+                pic : $scope.self.pic
+            });
+        }).error(function(data, status, headers, config) {});
+    };
+    $scope.load = function(id) {
             $http.get('').success(function(data) { //grab self
                 $scope.self = data;
             }).error(function(data) {
@@ -119,13 +130,13 @@
         };
         $scope.load();
     }]);
-    app.controller('EditProfileController', ['$scope', '$http', '$rootScope', function($scope, $http , $rootScope) {
+app.controller('EditProfileController', ['$scope', '$http', '$rootScope', function($scope, $http , $rootScope) {
 
 	$rootScope.bg = {'background-image' : 'url(/img/gray.jpg)'};
-        $scope.load = function() {
-            $http.get('/').success(function(data, status, headers, config) {
-                
-            }).error(function(data, status, headers, config) {
+    $scope.load = function() {
+        $http.get('/').success(function(data, status, headers, config) {
+
+        }).error(function(data, status, headers, config) {
                 //TODO "conn bad
 
                 data = {
@@ -134,9 +145,9 @@
                     company: "HackSC",
                     title: "Senior Executive Intern",
                     addresses: [
-                        "123 Easy St.",
-                        "Apt 44",
-                        "District 13"
+                    "123 Easy St.",
+                    "Apt 44",
+                    "District 13"
                     ],
                     city: 'San Jose',
                     state: 'CA',
@@ -157,89 +168,89 @@
                 };
                 $scope.profile = data;
             });
-        };
-        $scope.load();
-        $scope.submit = function() {
-            if ($scope.new_site_name != '' && $scope.new_site_url != '') {
-                $scope.profile.sites.push({
-                    name: $scope.new_site_name,
-                    url: $scope.new_site_url
-                });
-            }
-            if ($scope.new_phone_number != '') {
-                $scope.profile.phone_numbers.push($scope.new_phone_number);
-            }
-            if ($scope.new_address != '') {
-                $scope.profile.addresses.push($scope.new_address);
-            }
-            console.log($scope.profile);
-            $http.post('http://wontonst.com', {
-                profile: $scope.profile
-            }).success(function(data, status, headers, config) {
-                console.log(response);
+};
+$scope.load();
+$scope.submit = function() {
+    if ($scope.new_site_name != '' && $scope.new_site_url != '') {
+        $scope.profile.sites.push({
+            name: $scope.new_site_name,
+            url: $scope.new_site_url
+        });
+    }
+    if ($scope.new_phone_number != '') {
+        $scope.profile.phone_numbers.push($scope.new_phone_number);
+    }
+    if ($scope.new_address != '') {
+        $scope.profile.addresses.push($scope.new_address);
+    }
+    console.log($scope.profile);
+    $http.post('http://wontonst.com', {
+        profile: $scope.profile
+    }).success(function(data, status, headers, config) {
+        console.log(response);
                 $location.path('/profile'); //TODO GET PROFILES URL
             }).error(function(data, status, headers, config) {
                 $scope.emailTaken = true; //TODO maybe have two errors "conn bad" and "email taken"
             });
         };
     }]);
-    app.controller('RolodexController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {  
+app.controller('RolodexController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {  
 	
 	$rootScope.bg = {'background-image' : 'url(img/business.jpg)'};
-        $scope.cards = [];
-        $scope.load = function() {
-            $http.get('/').success(function(data, status, headers, config) {
-                console.log(data);
-            }).error(function(data, status, headers, config) {
-                data = [{
-                    name: 'Roy Zheng',
-                    phone_number: '333-333-3333',
-                    address: '123 Easy St.',
-                    city: 'San Luis Obispo',
-                    state: 'CA',
-                    country: 'USA',
-                    site: 'www.lame.com',
-                    email: 'roi@roy.com'
-                }, {
-                    name: 'James Flurry',
-                    site: 'www.lame.com',
-                    phone_number: '333-333-3333',
-                    address: '123 Easy St.',
-                    city: 'San Luis Obispo',
-                    state: 'CA',
-                    country: 'USA',
-                    email: 'roi@roy.com'
-                }, {
-                    name: 'Blake Trillington',
-                    site: 'www.lame.com',
-                    phone_number: '333-333-3333',
-                    address: '123 Easy St.',
-                    city: 'San Luis Obispo',
-                    state: 'CA',
-                    country: 'USA',
-                    email: 'roi@roy.com'
-                }, {
-                    name: 'Vlad Zhukov',
-                    site: 'www.lame.com',
-                    phone_number: '333-333-3333',
-                    address: '123 Easy St.',
-                    city: 'San Luis Obispo',
-                    state: 'CA',
-                    country: 'USA',
-                    email: 'roi@roy.com'
-                }, {
-                    name: 'Gilberto Lomberto Jose Luis Garcia de Oaxaca',
-                    site: 'www.lame.com',
-                    phone_number: '333-333-3333',
-                    address: '123 Easy St.',
-                    city: 'San Luis Obispo',
-                    state: 'CA',
-                    country: 'USA',
-                    email: 'roi@roy.com'
-                }];
-                $scope.cards = data;
-            });
-        };
-        $scope.load();
-    }]);
+    $scope.cards = [];
+    $scope.load = function() {
+        $http.get('/').success(function(data, status, headers, config) {
+            console.log(data);
+        }).error(function(data, status, headers, config) {
+            data = [{
+                name: 'Roy Zheng',
+                phone_number: '333-333-3333',
+                address: '123 Easy St.',
+                city: 'San Luis Obispo',
+                state: 'CA',
+                country: 'USA',
+                site: 'www.lame.com',
+                email: 'roi@roy.com'
+            }, {
+                name: 'James Flurry',
+                site: 'www.lame.com',
+                phone_number: '333-333-3333',
+                address: '123 Easy St.',
+                city: 'San Luis Obispo',
+                state: 'CA',
+                country: 'USA',
+                email: 'roi@roy.com'
+            }, {
+                name: 'Blake Trillington',
+                site: 'www.lame.com',
+                phone_number: '333-333-3333',
+                address: '123 Easy St.',
+                city: 'San Luis Obispo',
+                state: 'CA',
+                country: 'USA',
+                email: 'roi@roy.com'
+            }, {
+                name: 'Vlad Zhukov',
+                site: 'www.lame.com',
+                phone_number: '333-333-3333',
+                address: '123 Easy St.',
+                city: 'San Luis Obispo',
+                state: 'CA',
+                country: 'USA',
+                email: 'roi@roy.com'
+            }, {
+                name: 'Gilberto Lomberto Jose Luis Garcia de Oaxaca',
+                site: 'www.lame.com',
+                phone_number: '333-333-3333',
+                address: '123 Easy St.',
+                city: 'San Luis Obispo',
+                state: 'CA',
+                country: 'USA',
+                email: 'roi@roy.com'
+            }];
+            $scope.cards = data;
+        });
+};
+$scope.load();
+}]);
 })();
