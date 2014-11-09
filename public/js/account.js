17b2635d369e5da8a3e3d7f5cac4d1520804c433
Roy Zheng <roycraft3@gmail.com>
2014-11-09 03:54:18 -0800

    beautify

--- public/js/account.js
+++ public/js/account.js
@@ -1,8 +1,8 @@
 (function() {
-    var app = angular.module('qred-account', ['ngRoute','LocalStorageModule']);
+    var app = angular.module('qred-account', ['ngRoute', 'LocalStorageModule']);
     app.config(function($routeProvider, localStorageServiceProvider) {
-  localStorageServiceProvider
-    .setPrefix('qred');
+        localStorageServiceProvider
+            .setPrefix('qred');
         $routeProvider.when('/', {
                 templateUrl: '/directives/register.html',
                 controller: 'RegisterController'
@@ -12,19 +12,19 @@
                 controller: 'LoginController'
             });
     });
-    app.controller('LoginController', ['$scope', '$http', '$location', 'localStorageService', function($scope, $http, $location,localStorageService) {
+    app.controller('LoginController', ['$scope', '$http', '$location', 'localStorageService', function($scope, $http, $location, localStorageService) {
         $scope.emailTaken = false;
         $scope.submit = function() {
             $scope.emailTaken = false;
             console.log($scope.email);
             $http.post('/api/v1/token', {
-			'grant_type': 'password',
+                'grant_type': 'password',
                 username: $scope.email,
                 password: $scope.password
             }).success(function(data, status, headers, config) {
                 $location.path('/profile');
-				localStorageService.set('token',data.access_token);
-				localStorageService.set('user_id',data.user.uuid);
+                localStorageService.set('token', data.access_token);
+                localStorageService.set('user_id', data.user.uuid);
             }).error(function(data, status, headers, config) {
                 $scope.emailTaken = true; //TODO maybe have two errors "conn bad" and "email taken"
             });
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,33 +1,33 @@
 (function() {
-    var app = angular.module('qred-profile', ['ngRoute','LocalStorageModule']);
+    var app = angular.module('qred-profile', ['ngRoute', 'LocalStorageModule']);
     app.config(function($routeProvider, localStorageServiceProvider) {
-	
-  localStorageServiceProvider
-    .setPrefix('qred');
-	
+
+        localStorageServiceProvider
+            .setPrefix('qred');
+
         $routeProvider.when('/', {
-            templateUrl: '/directives/profile.html',
-        })
-        .when('/my', {
-            templateUrl: '/directives/mycard.html',
-        })
-        .when('/edit',{
-           templateUrl: '/directives/edit_profile.html',
-       })
-        .when('/wall',{
-           templateUrl: '/directives/wall.html',
-       });
+                templateUrl: '/directives/profile.html',
+            })
+            .when('/my', {
+                templateUrl: '/directives/mycard.html',
+            })
+            .when('/edit', {
+                templateUrl: '/directives/edit_profile.html',
+            })
+            .when('/wall', {
+                templateUrl: '/directives/wall.html',
+            });
     });
-	app.controller('BrowseController',['$scope','$rootScope','localStorageService', function($scope,$rootScope,localStorageService){
-	localStorageService.set('test','400');
-	console.log(localStorageService.keys());
-}]);
+    app.controller('BrowseController', ['$scope', '$rootScope', 'localStorageService', function($scope, $rootScope, localStorageService) {
+        localStorageService.set('test', '400');
+        console.log(localStorageService.keys());
+    }]);
 
     app.controller('ProfileController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
-	
-	$('#fuckthis').css('background-image' , 'url(/img/desk.jpg)');
-     $scope.profile = {};
-     $scope.load = function(id) {
+
+        $('#fuckthis').css('background-image', 'url(/img/desk.jpg)');
+        $scope.profile = {};
+        $scope.load = function(id) {
             $http.get('&id=' + id). //TODO get URL
             success(function(data, status, headers, config) {
                 $scope.profile = data;
@@ -40,9 +40,9 @@
                     company: "HackSC",
                     title: "Senior Executive Intern",
                     addresses: [
-                    "123 Easy St.",
-                    "Apt 44",
-                    "District 13"
+                        "123 Easy St.",
+                        "Apt 44",
+                        "District 13"
                     ],
                     city: 'San Jose',
                     state: 'CA',
@@ -60,7 +60,7 @@
                         url: 'http://youtube.com',
                         name: 'YouTube'
                     }],
-                    qrurl : "qred.azurewebsites.net/browse.html"
+                    qrurl: "qred.azurewebsites.net/browse.html"
 
                 };
                 $rootScope.profile = data;
@@ -80,26 +80,26 @@
                 vc = encodeURIComponent(vc);
 
                 $scope.profile.qrcontact = ("http://chart.apis.google.com/chart?cht=qr&chs=100x100&chl=" + vc);
-                
+
             });
-};
-$scope.load();
-}]);
-app.controller('WallController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
-    $scope.posts = [];
-    $scope.submit = function() {
-        $http.post('http://wontonst.com', {
-            content: $scope.new_post,
-            target: $rootScope.profile.email
-        }).success(function(data, status, headers, config) {
-            $scope.posts.push({
-                author: $scope.self.name,
+        };
+        $scope.load();
+    }]);
+    app.controller('WallController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
+        $scope.posts = [];
+        $scope.submit = function() {
+            $http.post('http://wontonst.com', {
                 content: $scope.new_post,
-                pic : $scope.self.pic
-            });
-        }).error(function(data, status, headers, config) {});
-    };
-    $scope.load = function(id) {
+                target: $rootScope.profile.email
+            }).success(function(data, status, headers, config) {
+                $scope.posts.push({
+                    author: $scope.self.name,
+                    content: $scope.new_post,
+                    pic: $scope.self.pic
+                });
+            }).error(function(data, status, headers, config) {});
+        };
+        $scope.load = function(id) {
             $http.get('').success(function(data) { //grab self
                 $scope.self = data;
             }).error(function(data) {
@@ -134,11 +134,11 @@ app.controller('WallController', ['$scope', '$rootScope', '$http', function($sco
         };
         $scope.load();
     }]);
-app.controller('EditProfileController', ['$scope', '$http', '$rootScope', function($scope, $http , $rootScope) {
-	$('#fuckthis').css('background-image' , 'url(/img/gray.jpg)');
+    app.controller('EditProfileController', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
+        $('#fuckthis').css('background-image', 'url(/img/gray.jpg)');
         $scope.load = function() {
             $http.get('/').success(function(data, status, headers, config) {
-                
+
             }).error(function(data, status, headers, config) {
                 //TODO "conn bad
 
@@ -148,9 +148,9 @@ app.controller('EditProfileController', ['$scope', '$http', '$rootScope', functi
                     company: "HackSC",
                     title: "Senior Executive Intern",
                     addresses: [
-                    "123 Easy St.",
-                    "Apt 44",
-                    "District 13"
+                        "123 Easy St.",
+                        "Apt 44",
+                        "District 13"
                     ],
                     city: 'San Jose',
                     state: 'CA',
@@ -171,89 +171,89 @@ app.controller('EditProfileController', ['$scope', '$http', '$rootScope', functi
                 };
                 $scope.profile = data;
             });
-};
-$scope.load();
-$scope.submit = function() {
-    if ($scope.new_site_name != '' && $scope.new_site_url != '') {
-        $scope.profile.sites.push({
-            name: $scope.new_site_name,
-            url: $scope.new_site_url
-        });
-    }
-    if ($scope.new_phone_number != '') {
-        $scope.profile.phone_numbers.push($scope.new_phone_number);
-    }
-    if ($scope.new_address != '') {
-        $scope.profile.addresses.push($scope.new_address);
-    }
-    console.log($scope.profile);
-    $http.post('http://wontonst.com', {
-        profile: $scope.profile
-    }).success(function(data, status, headers, config) {
-        console.log(response);
+        };
+        $scope.load();
+        $scope.submit = function() {
+            if ($scope.new_site_name != '' && $scope.new_site_url != '') {
+                $scope.profile.sites.push({
+                    name: $scope.new_site_name,
+                    url: $scope.new_site_url
+                });
+            }
+            if ($scope.new_phone_number != '') {
+                $scope.profile.phone_numbers.push($scope.new_phone_number);
+            }
+            if ($scope.new_address != '') {
+                $scope.profile.addresses.push($scope.new_address);
+            }
+            console.log($scope.profile);
+            $http.post('http://wontonst.com', {
+                profile: $scope.profile
+            }).success(function(data, status, headers, config) {
+                console.log(response);
                 $location.path('/profile'); //TODO GET PROFILES URL
             }).error(function(data, status, headers, config) {
                 $scope.emailTaken = true; //TODO maybe have two errors "conn bad" and "email taken"
             });
         };
     }]);
-app.controller('RolodexController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {  
-	
-	$('#fuckthis').css('background-image' , 'url(/img/business.jpg)');
-    $scope.cards = [];
-    $scope.load = function() {
-        $http.get('/').success(function(data, status, headers, config) {
-            console.log(data);
-        }).error(function(data, status, headers, config) {
-            data = [{
-                name: 'Roy Zheng',
-                phone_number: '333-333-3333',
-                address: '123 Easy St.',
-                city: 'San Luis Obispo',
-                state: 'CA',
-                country: 'USA',
-                site: 'www.lame.com',
-                email: 'roi@roy.com'
-            }, {
-                name: 'James Flurry',
-                site: 'www.lame.com',
-                phone_number: '333-333-3333',
-                address: '123 Easy St.',
-                city: 'San Luis Obispo',
-                state: 'CA',
-                country: 'USA',
-                email: 'roi@roy.com'
-            }, {
-                name: 'Blake Trillington',
-                site: 'www.lame.com',
-                phone_number: '333-333-3333',
-                address: '123 Easy St.',
-                city: 'San Luis Obispo',
-                state: 'CA',
-                country: 'USA',
-                email: 'roi@roy.com'
-            }, {
-                name: 'Vlad Zhukov',
-                site: 'www.lame.com',
-                phone_number: '333-333-3333',
-                address: '123 Easy St.',
-                city: 'San Luis Obispo',
-                state: 'CA',
-                country: 'USA',
-                email: 'roi@roy.com'
-            }, {
-                name: 'Gilberto Lomberto Jose Luis Garcia de Oaxaca',
-                site: 'www.lame.com',
-                phone_number: '333-333-3333',
-                address: '123 Easy St.',
-                city: 'San Luis Obispo',
-                state: 'CA',
-                country: 'USA',
-                email: 'roi@roy.com'
-            }];
-            $scope.cards = data;
-        });
-};
-$scope.load();
-}]);
+    app.controller('RolodexController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
+
+        $('#fuckthis').css('background-image', 'url(/img/business.jpg)');
+        $scope.cards = [];
+        $scope.load = function() {
+            $http.get('/').success(function(data, status, headers, config) {
+                console.log(data);
+            }).error(function(data, status, headers, config) {
+                data = [{
+                    name: 'Roy Zheng',
+                    phone_number: '333-333-3333',
+                    address: '123 Easy St.',
+                    city: 'San Luis Obispo',
+                    state: 'CA',
+                    country: 'USA',
+                    site: 'www.lame.com',
+                    email: 'roi@roy.com'
+                }, {
+                    name: 'James Flurry',
+                    site: 'www.lame.com',
+                    phone_number: '333-333-3333',
+                    address: '123 Easy St.',
+                    city: 'San Luis Obispo',
+                    state: 'CA',
+                    country: 'USA',
+                    email: 'roi@roy.com'
+                }, {
+                    name: 'Blake Trillington',
+                    site: 'www.lame.com',
+                    phone_number: '333-333-3333',
+                    address: '123 Easy St.',
+                    city: 'San Luis Obispo',
+                    state: 'CA',
+                    country: 'USA',
+                    email: 'roi@roy.com'
+                }, {
+                    name: 'Vlad Zhukov',
+                    site: 'www.lame.com',
+                    phone_number: '333-333-3333',
+                    address: '123 Easy St.',
+                    city: 'San Luis Obispo',
+                    state: 'CA',
+                    country: 'USA',
+                    email: 'roi@roy.com'
+                }, {
+                    name: 'Gilberto Lomberto Jose Luis Garcia de Oaxaca',
+                    site: 'www.lame.com',
+                    phone_number: '333-333-3333',
+                    address: '123 Easy St.',
+                    city: 'San Luis Obispo',
+                    state: 'CA',
+                    country: 'USA',
+                    email: 'roi@roy.com'
+                }];
+                $scope.cards = data;
+            });
+        };
+        $scope.load();
+    }]);
 })();
