b1963a05b61c66c13fd39f6910be1aa7519e4d2c
Roy Zheng <roycraft3@gmail.com>
2014-11-09 02:05:53 -0800

    fix

--- public/js/profile.js
+++ public/js/profile.js
@@ -20,6 +20,9 @@
 	}]);
     app.controller('ProfileController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
 	$rootScope.bg={'background-image' : 'url(/img/desk.jpg)'};
+	$scope.$on('$includeContentLoaded',function(){
+	console.log("fuck!");
+	});
         $scope.profile = {};
         $scope.load = function(id) {
             $http.get('&id=' + id). //TODO get URL
