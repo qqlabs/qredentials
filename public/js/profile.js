adcc9acffb545dfd70795e04c3c2640091538cd7
Roy Zheng <roycraft3@gmail.com>
2014-11-09 01:49:11 -0800

    fix

--- public/js/profile.js
+++ public/js/profile.js
@@ -115,6 +115,7 @@
     app.controller('EditProfileController', ['$scope', '$http', '$rootScope', function($scope, $http , $rootScope) {
 
 	$rootScope.bg = {'background-image' : 'url(img/gray.jpg)'};
+	$rootScope.$apply();
         $scope.load = function() {
             $http.get('/').success(function(data, status, headers, config) {
                 
