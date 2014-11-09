b3d8cba120659fe1901dbe4a6777c495a510b35e
Roy Zheng <roycraft3@gmail.com>
2014-11-09 03:30:54 -0800

    accounts api hookup

--- public/js/account.js
+++ public/js/account.js
@@ -12,17 +12,19 @@
                 controller: 'LoginController'
             });
     });
-    app.controller('LoginController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+    app.controller('LoginController', ['$scope', '$http', '$location', 'localStorageService', function($scope, $http, $location,localStorageService) {
         $scope.emailTaken = false;
         $scope.submit = function() {
             $scope.emailTaken = false;
             console.log($scope.email);
-            $http.post('http://wontonst.com', {
+            $http.post('/api/v1/token', {
+			'grant_type': 'password',
                 email: $scope.email,
                 password: $scope.password
             }).success(function(data, status, headers, config) {
-                console.log(response);
-                $location.path('/profile'); //TODO GET PROFILES URL
+                $location.path('/profile');
+				localStorageService.set('token',data.access_token);
+				localStorageService.set('user_id',data.user.uuid);
             }).error(function(data, status, headers, config) {
                 $scope.emailTaken = true; //TODO maybe have two errors "conn bad" and "email taken"
             });
@@ -30,11 +32,10 @@
     }]);
     app.controller('RegisterController', ['$scope', '$http', '$location', function($scope, $http, $location) {
         $scope.submit = function() {
-            $http.post('/res/account.php', {
+            $http.post('/api/v1/users', {
                 name: $scope.name,
                 email: $scope.email,
                 password: $scope.password,
-                action: "register"
             }).success(function(data, status, headers, config) {
                 console.log(data);
                 $location.path('/login'); //TODO GET PROFILES URL
